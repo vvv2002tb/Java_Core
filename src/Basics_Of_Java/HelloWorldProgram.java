15d5dd5b610a5bae178be246784dacdd805e76ea
Doan Trong Vinh <clonevinh95@gmail.com>
2024-07-23 00:09:26 +0700

    first java program

---
+++ src/Basics_Of_Java/HelloWorldProgram.java
@@ -0,0 +1,8 @@
+package Basics_Of_Java;
+//  In this article, we will learn how to write a simple Java Program.
+// Mọi chương trình Java phải có định nghĩa lớp khớp với tên tệp (tên lớp và tên tệp phải giống nhau).
+public class HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("My first Java Program.");
+    }
+}
