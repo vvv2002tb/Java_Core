525bf34918f8c65531e555752aa57e8d5723abbb
Doan Trong Vinh <clonevinh95@gmail.com>
2024-07-22 23:58:36 +0700

    the first lesson of basic syntax

---
+++ src/Basics_Of_Java/JavaBasicSyntax.java
@@ -0,0 +1,25 @@
+package Basics_Of_Java;
+
+public class JavaBasicSyntax {
+    //Example: Steps to compile and run a java program in a console
+    // javac Java_Basic_Syntax.java
+    // java Java_Basic_Syntax
+    public static void main(String[] args) {
+        System.out.println("GeeksforGeeks!");
+
+
+        // There e the types of comments in Java.
+        // this is a comment in java
+
+        /*
+            Multi-line Comment
+            System.out.println("This is the first line comment.");
+            System.out.println("This is the second line comment.");
+        */
+
+        /** documentation */
+
+
+
+    }
+}
