a4151dbbb11b1e14525e013cc47ae2a69b504a8a
Doan Trong Vinh <clonevinh95@gmail.com>
2024-07-24 18:31:33 +0700

    data types in java

---
+++ src/Basics_Of_Java/DataTypes.java
@@ -0,0 +1,32 @@
+package Basics_Of_Java;
+
+import java.util.Objects;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Kieu du lieu nguyen thuy
+        char a ='a';
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+        double d = 4.355121212;
+        float f = 4.542424f;
+        long l = 1221212;
+        System.out.println("char: " + a);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+
+        //Kiểu dữ liệu không nguyên thủy hoặc kiểu dữ liệu tham chiếu
+        //String
+        String string = "GeeksforGeeks";
+
+
+
+    }
+
+
+}
